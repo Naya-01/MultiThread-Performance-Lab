52c58dbfa9f6701e28882b0411e50e7bd66bd03a
Cedric Niyikiza <cedric.niyikiza@student.uclouvain.be>
2023-11-30 17:33:22 +0100

    add - task 2.2 - test and set

---
+++ P2/test_and_set/experiments.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#nombres de threads totaux
+thread_counts=(1 2 4 8 16 32 64)
+
+run_and_measure() {
+    program=$1
+    thread_count=$2
+
+    start_time=$(date +%s%N)
+    
+    ./$program $thread_count > /dev/null 2>&1
+
+    end_time=$(date +%s%N)
+
+    duration=$((($end_time - $start_time) / 1000000)) # millisecondes
+    echo "$thread_count, $duration" >> "./data/${program}_performance.csv"
+}
+
+program="test_and_set"
+echo "threads, duration_ms" > "./data/${program}_performance.csv"
+for count in "${thread_counts[@]}"; do
+    for i in {1..5}; do
+        run_and_measure $program $count
+    done
+done
+cat "./data/${program}_performance.csv"
+
+
+python3 "plots.py"
---
+++ P2/test_and_set/plots.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+def read_data(filename):
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        data = list(reader)
+    return np.array(data, dtype=int)
+
+def plot_data(program_name, data):
+    threads = np.unique(data[:, 0])
+    means = [np.mean(data[data[:, 0] == t, 1]) for t in threads]
+    std_devs = [np.std(data[data[:, 0] == t, 1]) for t in threads]
+
+    plt.errorbar(threads, means, yerr=std_devs, fmt='o-', capsize=5, label=program_name)
+    plt.xlabel('Nombre de Threads')
+    plt.xscale('log', base=2)
+    plt.xticks(threads, threads) 
+    plt.ylabel('Temps d\'exécution (ms)')
+    plt.title('Performance de ' + program_name)
+    plt.ylim(bottom=0)
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f"./plots/{program_name}.pdf")
+    plt.close()
+
+
+data = read_data("./data/test_and_set_performance.csv")
+plot_data("test_and_set", data)
+    
+
